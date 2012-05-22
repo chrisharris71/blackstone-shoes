ec66aee0fe33f66c6797f200dc7ec8970c5def68
Christopher <chris.harris71@yahoo.com>
2012-05-21 23:18:04 -0400

    Basic site layout started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackstone::Application.config.secret_token = '587d707595a09c9ed7c2626f7e3af34dcba6fb8e7f2783bd38fbd163a42d408dc22a71873738a44c5093fa4c400cbe5542340c59754bc35ff457efe7c5d934dd'
